7c7ff8c2d337022051a0059f2bae80b4bc7b142f
Evgeniy <vip.vlas.2000@mail.ru>
2023-02-26 20:29:34 +0300

    Commit 1

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,5 +1,29 @@
-import { IItem } from './index';
+import React, { useState } from 'react';
 
-export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
-    return <div></div>;
+export function Item(props: { name: string }) {
+    const [isEdit, setIsEdit] = useState(false);
+    const [name, setName] = useState(props.name);
+    const [editName, setEditName] = useState(props.name);
+
+    const keyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            setName(editName);
+            setIsEdit(false);
+        } else if (e.key === 'Escape') {
+            setIsEdit(false);
+        }
+    };
+
+    if (isEdit) {
+        return (
+            <input
+                type="text"
+                onKeyDown={keyDown}
+                defaultValue={name}
+                onChange={(e) => setEditName(e.target.value)}
+            />
+        );
+    } else {
+        return <div onClick={() => setIsEdit(true)}>{name}</div>;
+    }
 }
