8047da14dbece7e898d2f012a0ce5cc8501f770c
Evgeniy <vip.vlas.2000@mail.ru>
2023-02-26 20:36:00 +0300

    Commit 2

--- src/Keys.tsx
+++ src/Keys.tsx
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
+import { IItem } from './index';
+import { Item } from './Item';
 
-export function Item(props: { name: string }) {
-    const [isEdit, setIsEdit] = useState(false);
-    const [name, setName] = useState(props.name);
-    const [editName, setEditName] = useState(props.name);
-
-    const keyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter') {
-            setName(editName);
-            setIsEdit(false);
-        } else if (e.key === 'Escape') {
-            setIsEdit(false);
-        }
-    };
-
-    if (isEdit) {
-        return (
-            <input
-                type="text"
-                onKeyDown={keyDown}
-                defaultValue={name}
-                onChange={(e) => setEditName(e.target.value)}
-            />
-        );
+export function Keys(props: { initialData: IItem[]; sorting: 'ASC' | 'DESC' }) {
+    if (props.sorting === 'DESC') {
+        props.initialData.sort((a, b) => b.id - a.id);
     } else {
-        return <div onClick={() => setIsEdit(true)}>{name}</div>;
+        props.initialData.sort((a, b) => a.id - b.id);
     }
+
+    return (
+        <div>
+            {props.initialData.map((item) => (
+                <Item key={item.id} name={item.name} />
+            ))}
+        </div>
+    );
 }
